eb1fb44d92eb1e4664315c28915cdbcf3a5c1df9
PedroGarcia1227 <141671059+PedroGarcia1227@users.noreply.github.com>
2023-12-08 16:34:51 -0300

    Update dbCardapiosEitens.js

--- BACK-END/dbCardapiosEitens.js
+++ BACK-END/dbCardapiosEitens.js
@@ -1,33 +1,49 @@
-var config = require("./dbconfig")
+const config = require("./dbconfig")
 const sql = require("mssql");
-
+then
+//http://localhost:3000/selectLanches
 async function selectLanches(){
+    let pool;
     try{
-        let pool = await sql.connect(config);
+        pool = await sql.connect(config);
         let res = await pool.request().query("select * from SabDiv.itensCardapioLanches");
         return res.recordsets;
     } catch (error){
         console.log("erros: " + error)
+    } finally{
+        if(pool){
+            pool.close()
+        }
     }
 }
 
 async function selectBebidas(){
+    let pool;
     try{
-        let pool = await sql.connect(config);
+        pool = await sql.connect(config);
         let res = await pool.request().query("select * from SabDiv.itensCardapioBebidas");
         return res.recordsets;
     } catch (error){
         console.log("erros: " + error)
+    } finally{
+        if(pool){
+            pool.close()
+        }
     }
 }
 
 async function selectSobremesas(){
+    let pool;
     try{
-        let pool = await sql.connect(config);
-        let res = await pool.request().query("select * from SabDiv.itensCardapioSobremesas");
+        pool = await sql.connect(config);
+        let res = await pool.request().query("select * from SabDiv.Sobremesas");
         return res.recordsets;
     } catch (error){
         console.log("erros: " + error)
+    } finally{
+        if(pool){
+            pool.close()
+        }
     }
 }
 
