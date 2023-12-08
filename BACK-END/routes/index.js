6934fdca3a6bb29f813cdc1bd86986f59918ffdb
PedroGarcia1227 <141671059+PedroGarcia1227@users.noreply.github.com>
2023-12-08 16:33:21 -0300

    Update index.js

--- BACK-END/routes/index.js
+++ BACK-END/routes/index.js
@@ -1,20 +1,20 @@
 //RA 23331 - Pedro Henrique Garcia do Nascimento
 //RA 23338 -  Fabrício Carvalho Malagodi
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const sqlCardapiosEitens = require("../dbCardapiosEitens")
 const sqlClientes = require("../dbClientes")
 
 
-//curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "senha": ""}' http://localhost:3000/inserir-cliente
+// curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "senha": ""}' http://localhost:3000/inserir-cliente
 router.post('/inserir-cliente', async (req, res) => { 
   const { nomeUsuario, senha } = req.body;
   try {
     if (!nomeUsuario || !senha) {
       return res.status(400).json({ mensagem: 'Nome de usuário e senha são obrigatórios' });
     }
-    const resultado = await sqlClientes.inserirCliente(nomeUsuario, senha);
+    await sqlClientes.inserirCliente(nomeUsuario, senha);
     res.status(201).json({ mensagem: 'Cliente inserido com sucesso' });
   } catch (erro) {
     console.error(erro);
@@ -22,7 +22,7 @@ router.post('/inserir-cliente', async (req, res) => {
   }
 });
 
-//curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "senha": ""}' http://localhost:3000/validar-login
+// curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "senha": ""}' http://localhost:3000/validar-login
 router.post('/validar-login', async (req, res) => {
   const { nomeUsuario, senha } = req.body; 
   try {
@@ -41,22 +41,22 @@ router.post('/validar-login', async (req, res) => {
   }
 });
 
-//curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "Endereco": "", "CEP": ""}' http://localhost:3000/inserir-endereco-cliente
+// curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "Endereco": "", "CEP": ""}' http://localhost:3000/inserir-endereco-cliente
 router.post('/inserir-endereco-cliente', async (req, res) => { 
   const { nomeUsuario, Endereco, CEP } = req.body;
   try {
     if (!nomeUsuario || !Endereco || !CEP) {
       return res.status(400).json({ mensagem: 'Nome de usuário, endereço e CEP são obrigatórios' });
     }
-    const resultado = await sqlClientes.inserirEnderecoCliente(nomeUsuario, Endereco, CEP);
-    res.status(201).json({ mensagem: 'Endereço inserido com sucesso' });
+    await sqlClientes.inserirEnderecoCliente(nomeUsuario, Endereco, CEP);
+    res.status(201).json({ mensagem: 'Endereço inserido com sucesso'});
   } catch (erro) {
     console.error(erro);
     res.status(500).json({ mensagem: 'Erro ao inserir endereço' });
   }
 });
 
-//curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "metodoPagamento": ""}' http://localhost:3000/inserir-pedido
+// curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "metodoPagamento": ""}' http://localhost:3000/inserir-pedido
 router.post('/inserir-pedido', async (req, res) => {
   const { nomeUsuario, metodoPagamento } = req.body;
 
@@ -64,17 +64,15 @@ router.post('/inserir-pedido', async (req, res) => {
     if (!nomeUsuario || !metodoPagamento) {
       return res.status(400).json({ mensagem: 'Nome de usuário e método de pagamento são obrigatórios' });
     }
-
-    const resultado = await sqlClientes.inserirPedido(nomeUsuario, metodoPagamento);
-
-    res.status(201).json({ mensagem: 'Pedido inserido com sucesso', resultado });
+    await sqlClientes.inserirPedido(nomeUsuario, metodoPagamento);
+    res.status(201).json({ mensagem: 'Pedido inserido com sucesso'});
   } catch (erro) {
     console.error(erro);
     res.status(500).json({ mensagem: 'Erro no servidor ao inserir pedido' });
   }
 });
 
-//curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "metodoPagamento": "", nomeItem = "", quantidade = ""}' http://localhost:3000/inserir-item-pedido
+// curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": "", "metodoPagamento": "", nomeItem = "", quantidade = ""}' http://localhost:3000/inserir-item-pedido
 router.post('/inserir-item-pedido', async (req, res) => {
   const { nomeUsuario, nomeItem, quantidade } = req.body;
 
@@ -82,9 +80,7 @@ router.post('/inserir-item-pedido', async (req, res) => {
     if (!nomeUsuario || !nomeItem || !quantidade) {
       return res.status(400).json({ mensagem: 'Nome de usuário, nome do item e quantidade são obrigatórios' });
     }
-
     await sqlClientes.inserirItemPedido(nomeUsuario, nomeItem, quantidade);
-
     res.status(201).json({ mensagem: 'Item inserido ao pedido com sucesso' });
   } catch (erro) {
     console.error(erro);
@@ -92,7 +88,7 @@ router.post('/inserir-item-pedido', async (req, res) => {
   }
 });
 
-//curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": ""}' http://localhost:3000/calcular-e-inserir-preco-total
+// curl -X POST -H "Content-Type: application/json" -d '{"nomeUsuario": ""}' http://localhost:3000/calcular-e-inserir-preco-total
 router.post('/calcular-e-inserir-preco-total', async (req, res) => {
   const { nomeUsuario } = req.body;
 
