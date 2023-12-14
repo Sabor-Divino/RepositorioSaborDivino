813d41c34be6cdbf5691fba57a087dd6a823c7fc
PedroGarcia1227 <141671059+PedroGarcia1227@users.noreply.github.com>
2023-12-14 09:06:29 -0300

    Update Consulta.java

--- Interface Java/Consulta.java
+++ Interface Java/Consulta.java
@@ -1,17 +1,52 @@
 public class Consulta{
-    private int IdItemDeCardapio;
-    private String NomeItem;
-    private double Preco;
-    private int IdCardapio;
+    private int idItemDeCardapio;
+    private String nomeItem;
+    private double preco;
+    private int idCardapio;
 
-    public Consulta(int IdItemDeCardapio, String NomeItem, double Preco, int IdCardapio) {
-        this.IdItemDeCardapio = IdItemDeCardapio;
-        this.NomeItem = NomeItem;
-        this.Preco = Preco;
-        this.IdCardapio = IdCardapio;
+    public Consulta(int idItemDeCardapio, String nomeItem, double preco, int idCardapio) throws Exception{
+        if (idItemDeCardapio == 0) throw new Exception("Id item inválido");
+        if (idCardapio == 0) throw new Exception("Id cardápio inválido");
+        if(nomeItem.equals("")) throw new Exception("Nome item inválido");
+        this.idItemDeCardapio = idItemDeCardapio;
+        this.nomeItem = nomeItem;
+        this.preco = preco;
+        this.idCardapio = idCardapio;
     }
 
     public Object[] toArray() {
-        return new Object[]{IdItemDeCardapio, NomeItem, Preco, IdCardapio};
+        return new Object[]{idItemDeCardapio, nomeItem, preco, idCardapio};
+    }
+
+    @Override
+    public String toString(){
+        String ret = "Id item: "+this.idItemDeCardapio+"\nNome item: "+this.nomeItem+"\nPreço: "+this.preco+"\nId cardápio: "+this.idCardapio;
+        return ret;
+    }
+
+    @Override
+    public int hashCode(){
+        int ret = 1;
+
+        ret += ret * 7 + Integer.valueOf(this.idItemDeCardapio).hashCode();
+        ret += ret * 7 + String.valueOf(this.nomeItem).hashCode();
+        ret += ret * 7 + Double.valueOf(this.preco).hashCode();
+        ret += ret * 7 + Integer.valueOf(this.idCardapio).hashCode();
+
+        return ret;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null) return false;
+        if(obj == this) return true;
+        if(obj.getClass() != this.getClass()) return false;
+
+        Consulta con = (Consulta) (obj);
+        if(con.idItemDeCardapio != this.idItemDeCardapio) return false;
+        if(con.nomeItem != this.nomeItem) return false;
+        if(con.preco != this.preco) return false;
+        if(con.idCardapio != this.idCardapio) return false;
+        return true;
     }
 }
